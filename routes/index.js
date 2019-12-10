0b3b0e607484efc48d07b6bc4cf66993ca50778a
jeonghj2050 <qjrm52@naver.com>
2019-12-10 21:06:07 +0900

    수정중

--- routes/index.js
+++ routes/index.js
@@ -9,19 +9,44 @@ const Seat = require("../models/Seat").Seat
 router.get('/',function(req,res){
     session =req.session;
     Seat.find({},function(req,seat){
-        console.log(seat);
+        //console.log(seat);
         res.render('index',{session:session,seat:seat})
     })
 });
+
 // 좌석 예매
-router.post('/reserve', function(req, res, next){
+router.get('/reserve/:listId', function (req, res, next) {
     session = req.session;
-    Seat.find({}, function(err, seat){
-        console.log(seat);
-        //res.render('main', {title:'Express', seat: seat });
-        res.render('index', { session:session, seat: seat });
-        
+    var seatId = req.params.listId;
+    console.log(seatId);
+    Seat.updateMany({_id: seatId},{status:2}, function (err, result) {
+        res.redirect('/');
     });
+})
+
+// 좌석 예매 취소
+router.get('/cancel/:listId', function (req, res, next) {
+    session = req.session;
+    var seatId = req.params.listId;
+    //console.log(seatId);
+    Seat.updateMany({_id: seatId},{status:0}, function (err, result) {
+        res.redirect('/');
+    });
+})
+
+// 좌석 조회
+router.post('/searchSeat', function (req, res, next) {
+    let session = req.session;
+
+console.log(req.body.departure_country);
+console.log(req.body.arrival_country);
+    Seat.find({ 
+        departure_country: req.body.departure_country,
+        arrival_Country: req.body.arrival_country
+     }, (err, seat) => {
+         console.log(seat);
+        res.render('index',{session:session, seat:seat})
+    })
 });
 
 router.use('/question',Question);
